a288c87e53799bbc6f0b6acb0da7fde9afc350de
WongJeMoment <wz030517@gmail.com>
2025-04-21 23:17:21 +0800

    add val

--- models/config.py
+++ models/config.py
@@ -5,5 +5,7 @@ BATCH_SIZE = 16
 EPOCHS = 50
 LR = 1e-3
 
-TRAIN_IMAGE_DIR = "/home/wangzhe/ICRA2025/MY/data/frame_resized"
-TRAIN_LABEL_DIR = "/home/wangzhe/ICRA2025/MY/data/heatmaps"
+TRAIN_IMAGE_DIR = "/home/wangzhe/ICRA2025/MY/data/train_frame"
+TRAIN_LABEL_DIR = "/home/wangzhe/ICRA2025/MY/data/train_heatmaps"
+VAL_IMAGE_DIR = "/home/wangzhe/ICRA2025/MY/data/train_frame"
+VAL_LABEL_DIR = "/home/wangzhe/ICRA2025/MY/data/val_heatmaps"
