957367b3b1fd1c495be15b407df6c97d7dbeb626
WongJeMoment <wz030517@gmail.com>
2025-04-21 13:36:42 +0800

    json2npy

--- models/config.py
+++ models/config.py
@@ -1,10 +1,9 @@
 # config.py
 IMG_SIZE = 256
-NUM_KEYPOINTS = 8
 SIGMA = 5
 BATCH_SIZE = 16
 EPOCHS = 50
 LR = 1e-3
 
-TRAIN_IMAGE_DIR = "./data/train/images"
-TRAIN_LABEL_DIR = "./data/train/jsons"
+TRAIN_IMAGE_DIR = "/home/wangzhe/ICRA2025/MY/data/frame_resized"
+TRAIN_LABEL_DIR = "/home/wangzhe/ICRA2025/MY/data/heatmaps"
--- models/dataset.py
+++ models/dataset.py
@@ -1,22 +1,35 @@
 # dataset.py
-import os, json
+import os
+import json
 import torch
 import cv2
+import numpy as np
 from torch.utils.data import Dataset
 from config import IMG_SIZE, SIGMA
 from utils import generate_heatmap
 
+def infer_num_keypoints(json_dir):
+    sample_file = sorted([f for f in os.listdir(json_dir) if f.endswith('.json')])[0]
+    with open(os.path.join(json_dir, sample_file)) as f:
+        keypoints = json.load(f)
+    return len(keypoints)
+
 class KeypointDataset(Dataset):
-    def __init__(self, image_dir, json_dir, transform=None):
-        self.image_paths = sorted([os.path.join(image_dir, f) for f in os.listdir(image_dir)])
-        self.json_paths = sorted([os.path.join(json_dir, f) for f in os.listdir(json_dir)])
-        self.transform = transform
+    def __init__(self, image_dir, json_dir):
+        self.image_paths = sorted([
+            os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.endswith((".jpg", ".png"))
+        ])
+        self.json_paths = sorted([
+            os.path.join(json_dir, os.path.splitext(os.path.basename(f))[0] + '.json')
+            for f in os.listdir(image_dir) if f.endswith((".jpg", ".png"))
+        ])
 
     def __len__(self):
         return len(self.image_paths)
 
     def __getitem__(self, idx):
         img = cv2.imread(self.image_paths[idx])
+        h0, w0 = img.shape[:2]
         img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
         img = img.astype('float32') / 255.0
         img = img.transpose(2, 0, 1)  # HWC -> CHW
@@ -24,5 +37,12 @@ class KeypointDataset(Dataset):
         with open(self.json_paths[idx]) as f:
             keypoints = np.array(json.load(f), dtype=np.float32)
 
+        # 坐标按原图分辨率缩放
+        scale_x = IMG_SIZE / w0
+        scale_y = IMG_SIZE / h0
+        keypoints[:, 0] *= scale_x
+        keypoints[:, 1] *= scale_y
+
         heatmaps = generate_heatmap(keypoints, IMG_SIZE, SIGMA)
+
         return torch.tensor(img), torch.tensor(heatmaps)
