48c2412d4d1607b0fcc014bcc1004f11a0425979
WongJeMoment <wz030517@gmail.com>
2025-04-21 22:21:53 +0800

    change network 2 Unet

--- models/model.py
+++ models/model.py
@@ -1,23 +1,71 @@
-# model.py
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+class DoubleConv(nn.Module):
+    """两个连续的卷积 + BN + ReLU"""
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
 
 class HeatmapNet(nn.Module):
     def __init__(self, num_keypoints):
         super().__init__()
-        self.encoder = nn.Sequential(
-            nn.Conv2d(3, 32, 3, 1, 1), nn.ReLU(),
-            nn.Conv2d(32, 64, 3, 1, 1), nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Conv2d(64, 128, 3, 1, 1), nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(128, 64, 2, stride=2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(64, num_keypoints, 2, stride=2)
-        )
+
+        # 编码器
+        self.enc1 = DoubleConv(3, 64)
+        self.pool1 = nn.MaxPool2d(2)
+        self.enc2 = DoubleConv(64, 128)
+        self.pool2 = nn.MaxPool2d(2)
+        self.enc3 = DoubleConv(128, 256)
+        self.pool3 = nn.MaxPool2d(2)
+        self.enc4 = DoubleConv(256, 512)
+        self.pool4 = nn.MaxPool2d(2)
+
+        # 中间层
+        self.bottleneck = DoubleConv(512, 1024)
+
+        # 解码器
+        self.up4 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.dec4 = DoubleConv(1024, 512)
+
+        self.up3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.dec3 = DoubleConv(512, 256)
+
+        self.up2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.dec2 = DoubleConv(256, 128)
+
+        self.up1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.dec1 = DoubleConv(128, 64)
+
+        # 输出层：K 个关键点通道
+        self.final = nn.Conv2d(64, num_keypoints, kernel_size=1)
 
     def forward(self, x):
-        x = self.encoder(x)
-        x = self.decoder(x)
-        return x  # shape: [B, K, H, W]
+        # 编码
+        e1 = self.enc1(x)
+        e2 = self.enc2(self.pool1(e1))
+        e3 = self.enc3(self.pool2(e2))
+        e4 = self.enc4(self.pool3(e3))
+
+        # 中间
+        b = self.bottleneck(self.pool4(e4))
+
+        # 解码 + 拼接
+        d4 = self.dec4(torch.cat([self.up4(b), e4], dim=1))
+        d3 = self.dec3(torch.cat([self.up3(d4), e3], dim=1))
+        d2 = self.dec2(torch.cat([self.up2(d3), e2], dim=1))
+        d1 = self.dec1(torch.cat([self.up1(d2), e1], dim=1))
+
+        out = self.final(d1)
+        return out  # shape: [B, K, H, W]
