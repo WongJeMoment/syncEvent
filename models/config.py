6596cea224816df312273c9f7efda2b382e80b5c
WongJeMoment <wz030517@gmail.com>
2025-04-22 09:10:17 +0800

    add val

--- models/config.py
+++ models/config.py
@@ -7,5 +7,5 @@ LR = 1e-3
 
 TRAIN_IMAGE_DIR = "/home/wangzhe/ICRA2025/MY/data/train_frame"
 TRAIN_LABEL_DIR = "/home/wangzhe/ICRA2025/MY/data/train_heatmaps"
-VAL_IMAGE_DIR = "/home/wangzhe/ICRA2025/MY/data/train_frame"
+VAL_IMAGE_DIR = "/home/wangzhe/ICRA2025/MY/data/val_frame"
 VAL_LABEL_DIR = "/home/wangzhe/ICRA2025/MY/data/val_heatmaps"
