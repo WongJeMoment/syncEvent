6502519e49f06abd1db33f554a883aef21c265c3
WongJeMoment <wz030517@gmail.com>
2025-04-24 22:41:49 +0800

    config

--- ContourDetection/config.py
+++ ContourDetection/config.py
@@ -1,9 +1,10 @@
 # config.py
 IMG_SIZE = 256
 BATCH_SIZE = 8
-EPOCHS = 30
+EPOCHS = 10
 LR = 1e-3
 
 IMAGE_DIR = "/home/wangzhe/ICRA2025/MY/data/train_frame"
 EDGE_DIR = "/home/wangzhe/ICRA2025/MY/data/train_canny"
 CHECKPOINT_PATH = "/home/wangzhe/ICRA2025/MY/models/edge/edgenet.pt"
+VAL_IMAGE_DIR = "/home/wangzhe/ICRA2025/MY/models/visual_results"
