3a75a52db0107c5f40f3896580e230f3b2f94331
WongJeMoment <wz030517@gmail.com>
2025-04-24 14:50:08 +0800

    canny

--- ContourDetection/config.py
+++ ContourDetection/config.py
@@ -4,6 +4,6 @@ BATCH_SIZE = 8
 EPOCHS = 30
 LR = 1e-3
 
-IMAGE_DIR = ""
-EDGE_DIR = ""
-CHECKPOINT_PATH = ""
+IMAGE_DIR = "/home/wangzhe/ICRA2025/MY/data/train_frame"
+EDGE_DIR = "/home/wangzhe/ICRA2025/MY/data/train_canny"
+CHECKPOINT_PATH = "/home/wangzhe/ICRA2025/MY/models/edge/edgenet.pt"
