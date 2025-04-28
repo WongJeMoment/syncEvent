d02fd6a1392706542229454611483e3188d52a17
WongJeMoment <wz030517@gmail.com>
2025-04-28 13:22:23 +0800

    Real Camera Insider

--- 2D-3D/camera_config.py
+++ 2D-3D/camera_config.py
@@ -4,9 +4,10 @@ def get_camera_matrix(image_width, image_height):
     """
     给定图像尺寸，生成相机内参矩阵
     """
-    fx = fy = 1200  # 可以根据需要调整
-    cx = image_width / 2
-    cy = image_height / 2
+    fx = 1761.63991067404
+    fy = 1763.02728468010  # 可以根据需要调整
+    cx = 672.771832431268
+    cy = 364.906730287696
 
     camera_matrix = np.array([
         [fx, 0, cx],
