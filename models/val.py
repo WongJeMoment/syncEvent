dc1fb3a429656ab390a619de905479a4014a9d63
WongJeMoment <wz030517@gmail.com>
2025-05-09 11:35:57 +0800

    val_change image size

--- models/val.py
+++ models/val.py
@@ -5,15 +5,14 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-from model import HeatmapUNet  # 或 HeatmapNet
-from config import VAL_IMAGE_DIR
+from config import IMG_SIZE, VAL_IMAGE_DIR
+from model import HybridHeatmapUNet  # 或 HeatmapNet
 
 
 def load_image(img_path):
     img = cv2.imread(img_path)
-    orig_h, orig_w = img.shape[:2]
-    img_tensor = torch.from_numpy(img).permute(2, 0, 1).float() / 255.0  # C, H, W
-    return img_tensor.unsqueeze(0), (orig_w, orig_h), img  # (1, C, H, W), 原始尺寸, 原始图像
+    img = torch.from_numpy(img).permute(2, 0, 1).float() / 255.0
+    return img.unsqueeze(0)  # B, C, H, W
 
 
 def extract_peak_coords(heatmap_tensor):
@@ -25,18 +24,19 @@ def extract_peak_coords(heatmap_tensor):
     return coords
 
 
-def visualize_prediction(orig_img, pred_heatmap):
+def visualize_prediction(img_tensor, pred_heatmap):
+    img = img_tensor.squeeze(0).permute(1, 2, 0).cpu().numpy()
     pred_vis = pred_heatmap.squeeze(0).sum(dim=0).detach().cpu().numpy()
     pred_coords = extract_peak_coords(pred_heatmap)
 
     plt.figure(figsize=(10, 4))
 
     plt.subplot(1, 2, 1)
-    plt.title("Original Image")
-    plt.imshow(cv2.cvtColor(orig_img, cv2.COLOR_BGR2RGB))
+    plt.title("Image")
+    plt.imshow(img)
 
     plt.subplot(1, 2, 2)
-    plt.title("Predicted Heatmap")
+    plt.title("Predicted Keypoints")
     plt.imshow(pred_vis, cmap='hot')
     for i, (x, y) in enumerate(pred_coords):
         plt.scatter(x, y, color='lime', s=30)
@@ -46,11 +46,11 @@ def visualize_prediction(orig_img, pred_heatmap):
     plt.show()
 
 
-def predict_images(model_path="checkpoints/best_model_part2.pt", image_dir=VAL_IMAGE_DIR, num_keypoints=15):
+def predict_images(model_path="checkpoints/best_model.pt", image_dir=VAL_IMAGE_DIR, num_keypoints=15):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = HeatmapUNet(num_keypoints=num_keypoints).to(device)
-    model.eval()
+    model = HybridHeatmapUNet(num_keypoints=num_keypoints).to(device)
     model.load_state_dict(torch.load(model_path, map_location=device))
+    model.eval()
 
     image_paths = sorted([
         os.path.join(image_dir, f) for f in os.listdir(image_dir)
@@ -59,13 +59,9 @@ def predict_images(model_path="checkpoints/best_model_part2.pt", image_dir=VAL_I
 
     with torch.no_grad():
         for img_path in image_paths:
-            img_tensor, (w, h), orig_img = load_image(img_path)
-            img_tensor = img_tensor.to(device)
-
+            img_tensor = load_image(img_path).to(device)
             pred = model(img_tensor)
-            pred = F.interpolate(pred, size=(h, w), mode='bilinear', align_corners=False)
-
-            visualize_prediction(orig_img, pred.cpu())
+            visualize_prediction(img_tensor.cpu(), pred.cpu())
 
 
 if __name__ == "__main__":
